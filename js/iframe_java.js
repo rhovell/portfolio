9bdb0e3d58bd77aadb6ec0705c3e508de209396c
Rachel Hovell <r.hovell@live.com>
2018-06-06 11:11:33 +0100

    Added scroll to top button to iframe pages

--- js/iframe_java.js
+++ js/iframe_java.js
@@ -101,3 +101,21 @@ function scroller(event) {
       return false;
     }
 };
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
+        document.getElementById("scrollUp").style.display = "block";
+    } else {
+        document.getElementById("scrollUp").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
